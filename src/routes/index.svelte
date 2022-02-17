afa5ca63cfdfa1988e578fe22196da221f604bd1
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2022-02-16 23:28:53 -0500

    Initialisation

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+
+<div class="py-20 md:py-28">
+    <div class="container px-4 mx-auto">
+        <div class="flex flex-wrap xl:items-center -mx-4">
+            <div class="w-full md:w-1/2 px-4 mb-16 md:mb-0">
+                <span class="inline-block py-px px-2 mb-4 text-xs leading-5 text-white bg-blue-500 uppercase rounded-9xl">Accueil</span>
+                <h1 class="mb-6 text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight">Développeur, humaticien, passionné</h1>
+                <p class="mb-8 text-lg md:text-xl text-coolGray-500 font-medium">Candidat à la maîtrise, entrepreneur, développeur web et passionné par les Humanités numériques. Découvrez mon petit monde!</p>
+                <div class="flex flex-wrap">
+                    <div class="w-full md:w-auto py-1 md:py-0 md:mr-4"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-blue-50 font-medium text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border border-blue-500 rounded-md shadow-sm" href="#">Lire mon blog</a></div>
+                    <div class="w-full md:w-auto py-1 md:py-0"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-coolGray-800 font-medium text-center bg-white hover:bg-coolGray-100 focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm" href="#">Voir mon portfolio</a></div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 px-4">
+                <div class="relative mx-auto md:mr-0 w-3/4">
+                    <img src="/jv.jpg" class="rounded-sm">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
